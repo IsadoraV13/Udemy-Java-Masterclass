eff9cb9aeb63c9bd7de775dcb6f059438c80afc9
Isadora <isadora@Isadoras-Air.home>
2023-04-12 19:56:44 +0200

    Added exercises 1-5

---
+++ Section5_CodingExercise/src/BarkingDog.java
@@ -0,0 +1,19 @@
+public class BarkingDog {
+
+    public static void main(String[] args) {
+        System.out.println("1 " + shouldWakeUp(true, 1));
+        System.out.println("2 " +shouldWakeUp(false, 2));
+        System.out.println("3 " +shouldWakeUp(true, 8));
+        System.out.println("4 " +shouldWakeUp(true, -1));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking || hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else if (hourOfDay < 8 || hourOfDay > 22) {
+            //barking at night
+            return true;
+        } else
+            // barking during the day
+            return false;
+    }
+}
---
+++ Section5_CodingExercise/src/SpeedConverter.java
@@ -0,0 +1,40 @@
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.114));
+
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        long milesPerHour = -1;
+        double kilometerToMileConversion = 1.609;
+        if (kilometersPerHour < 0) {
+            return milesPerHour;
+        } else if (kilometersPerHour >= 0) {
+            milesPerHour = Math.round(kilometersPerHour / kilometerToMileConversion);
+        }
+        return milesPerHour;
+    }
+
+    public static void printConversion (double kilometersPerHour) {
+        long milesPerHour = -1;
+        double kilometerToMileConversion = 1.609;
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");;
+        } else if (kilometersPerHour >= 0) {
+            milesPerHour = Math.round(kilometersPerHour / kilometerToMileConversion);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+
+    }
+}
